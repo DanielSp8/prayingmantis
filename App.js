b274f3b8cf1d8465c44db4c88aa5b9d01284a1b7
Daniel Speight <danisp1@mail.regent.edu>
2023-03-13 07:38:51 -0500

    Navigation from a HomeScreen/Menu Working

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Identity from "./Components/Identity";
 import Unreached from "./Components/Unreached";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.menuText}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Identity in Christ"
+        onPress={() => navigation.navigate("Identity")}
+      />
+      <Button
+        title="Unreached of the Day"
+        onPress={() => navigation.navigate("Unreached")}
+      />
+    </View>
+  );
+};
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Identity" component={Identity} />
         <Stack.Screen name="Unreached" component={Unreached} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  menuText: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default App;
