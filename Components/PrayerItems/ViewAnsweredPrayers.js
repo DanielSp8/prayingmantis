10c4bcd132fd21a295c3109c330aca99d3a43c08
Daniel Speight <danisp1@mail.regent.edu>
2023-08-05 13:50:11 -0500

    Got a lot accomplished on the Viewed Answered Prayers component!

--- Components/PrayerItems/ViewAnsweredPrayers.js
+++ Components/PrayerItems/ViewAnsweredPrayers.js
@@ -6,18 +6,59 @@ import {
   Button,
   ImageBackground,
   Pressable,
+  Text,
+  SafeAreaView,
 } from "react-native";
-import { Card, Text } from "@rneui/themed";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WrappedText from "react-native-wrapped-text";
+import { Card } from "@rneui/themed";
 import axios from "axios";
-// import ReadPrayerList from "../src/api/ReadPrayerList";
+import ReadPrayerList from "../../src/api/ReadPrayerList";
 import RandomBackgroundNatureImage from "../../docs/BackgroundNatureImages02";
 
 class ViewAnsweredPrayers extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      answeredPrayers: [],
+    };
+
+    this.getAnsweredPrayersToDisplay =
+      this.getAnsweredPrayersToDisplay.bind(this);
+    this.getPrayerListData = this.getPrayerListData.bind(this);
+  }
+
+  getAnsweredPrayersToDisplay = (array) => {
+    var i = 1;
+
+    return array.map((element) => {
+      return (
+        <View key={i++}>
+          <WrappedText style={styles.prayerListText}>{element}</WrappedText>
+        </View>
+      );
+    });
+  };
+
+  getPrayerListData = async () => {
+    const response = await ReadPrayerList.get("/prayerlists");
+
+    let answeredPrayerListInfo = response.data[0];
+
+    if (answeredPrayerListInfo.answeredPrayerRequests) {
+      this.setState({
+        answeredPrayers: answeredPrayerListInfo.answeredPrayerRequests,
+      });
+    } else {
+      this.setState({
+        answeredPrayers: [
+          "Keep praying!  God hears you!  He will answer you!  Persevere!",
+        ],
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.getPrayerListData();
   }
 
   render() {
@@ -26,9 +67,18 @@ class ViewAnsweredPrayers extends Component {
         source={RandomBackgroundNatureImage}
         style={styles.backgroundImage}
       >
-        <View>
-          <Text>This is a test of the View Answered Prayers Component.</Text>
-        </View>
+        <SafeAreaView>
+          <View>
+            <Card>
+              <View>
+                <Text style={styles.textHeading}>Answered Prayers:</Text>
+                {/* <WrappedText> */}
+                {this.getAnsweredPrayersToDisplay(this.state.answeredPrayers)}
+                {/* </WrappedText> */}
+              </View>
+            </Card>
+          </View>
+        </SafeAreaView>
       </ImageBackground>
     );
   }
@@ -40,6 +90,21 @@ const styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
     opacity: 0.85,
+  },
+  textHeading: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  containerForText: {
+    // flexDirection: "column",
+    // flex: 0.8,
+  },
+  prayerListText: {
+    color: "black",
+    fontSize: 13,
+    textAlign: "center",
+    flex: 1,
+    flexWrap: "wrap",
   },
 });
 
