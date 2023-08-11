20e832e3243759147348ebcdbaf25d2e41d10344
Daniel Speight <danisp1@mail.regent.edu>
2023-08-11 17:42:48 -0500

    I've come far.  Currently working on Axios put request to update MongoDB.

--- Components/PrayerItems/EditPrayerList.js
+++ Components/PrayerItems/EditPrayerList.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
-import { useState } from "react";
 import {
   View,
   StyleSheet,
-  Button,
   ImageBackground,
   Pressable,
   TextInput,
 } from "react-native";
 import { Card, Text } from "@rneui/themed";
-import WrappedText from "react-native-wrapped-text";
-import axios from "axios";
 import ReadPrayerList from "../../src/api/ReadPrayerList";
 import RandomBackgroundNatureImage from "../../docs/BackgroundNatureImages02";
 
@@ -18,22 +14,41 @@ class EditPrayerList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      prayerTheme: "",
       prayerList: [],
       numOfPrayerRequests: 0,
-      prayerInputsDisplaying: "",
+      currentPrayerRequestNum: 0,
     };
 
     this.getPrayerListData = this.getPrayerListData.bind(this);
     this.getPrayersToDisplay = this.getPrayersToDisplay.bind(this);
+    this.updateTheme = this.updateTheme.bind(this);
+    this.updateRequestsInState = this.updateRequestsInState.bind(this);
+    this.updatePrayerRequestsAction =
+      this.updatePrayerRequestsAction.bind(this);
+    this.skipToTheNextPrayerRequest =
+      this.skipToTheNextPrayerRequest.bind(this);
+    this.skipToThePreviousPrayerRequest =
+      this.skipToThePreviousPrayerRequest.bind(this);
+    this.updatePrayerListInMongo = this.updatePrayerListInMongo.bind(this);
   }
 
   getPrayersToDisplay = (array) => {
-    var i = 1;
-
+    var i = -1;
     return array.map((element) => {
+      i++;
+      // console.log(element);
       return (
-        <View key={i++}>
-          <TextInput style={styles.prayerInputBox}>{element}</TextInput>
+        <View key={i}>
+          <TextInput
+            name={i}
+            placeholder={element}
+            onSubmitEditing={(text) => {
+              this.updateRequestsInState(element, text.nativeEvent.text);
+            }}
+            style={styles.prayerInputBox}
+          />
+          <Text></Text>
         </View>
       );
     });
@@ -42,29 +57,79 @@ class EditPrayerList extends Component {
   getPrayerListData = async () => {
     const response = await ReadPrayerList.get("/prayerlists");
 
-    let prayerInputsToDisplay = "";
     let prayerListInfo = response.data[0];
+
     let prayerRequestsNum = prayerListInfo.prayerRequests.length - 1;
 
+    let prayerInfo =
+      prayerListInfo.prayerRequests[this.state.currentPrayerRequestNum];
+    let prayerThemeInfo = prayerInfo.splice(0, 1);
+    let thePrayerTheme = prayerThemeInfo.toString();
+
     this.setState({
-      prayerList: prayerListInfo.prayerRequests,
+      prayerTheme: thePrayerTheme,
+      prayerList: prayerInfo,
       numOfPrayerRequests: prayerRequestsNum,
     });
+  };
 
-    // for (let i = 0; i < this.state.numOfPrayerRequests; i++) {
-    //   prayerInputsToDisplay = prayerInputsToDisplay + this.getPrayerListData(i);
-    // }
+  skipToTheNextPrayerRequest = () => {
+    if (this.state.numOfPrayerRequests > this.state.currentPrayerRequestNum) {
+      this.setState({
+        currentPrayerRequestNum: this.state.currentPrayerRequestNum + 1,
+      });
+      this.getPrayerListData();
+    } else {
+      console.log(
+        `You've prayed through the prayer requests... ${this.state.currentPrayerRequestNum}`
+      );
+    }
+  };
 
-    //Goal:  Find out the array I want to pass into the getPrayersToDisplay function.
-    console.log(`this.state.prayerList: ${this.state.prayerList}`);
+  skipToThePreviousPrayerRequest = () => {
+    if (this.state.currentPrayerRequestNum > 0) {
+      this.setState({
+        currentPrayerRequestNum: this.state.currentPrayerRequestNum - 1,
+      });
+      this.getPrayerListData();
+    } else {
+      console.log(
+        `You're at the beginning of your prayer requests: ${this.state.currentPrayerRequestNum}`
+      );
+    }
+  };
 
-    // prayerInputsToDisplay = this.getPrayerListData(
-    //   prayerListInfo.prayerRequests[0]
-    // );
-    // this.setState({
-    //   prayerInputsDisplaying: prayerInputsToDisplay,
-    // });
-    // console.log(`prayerInputsToDisplay: ${prayerInputsToDisplay}`);
+  updateTheme = (newTheme) => {
+    this.setState({ prayerTheme: newTheme });
+  };
+
+  updateRequestsInState = (oldElement, newElement) => {
+    let iterationOfPrayerList = this.state.prayerList.indexOf(oldElement);
+    let updatedPrayerList = this.state.prayerList;
+    updatedPrayerList[iterationOfPrayerList] = newElement;
+
+    this.setState({
+      prayerList: updatedPrayerList,
+    });
+  };
+
+  updatePrayerRequestsAction = () => {
+    let arrayToUpdate = this.state.prayerList;
+    arrayToUpdate.unshift(this.state.prayerTheme);
+    console.log(arrayToUpdate);
+    this.updatePrayerListInMongo(arrayToUpdate);
+  };
+
+  updatePrayerListInMongo = async (arrayToUpdate) => {
+    const response = await ReadPrayerList.get("/prayerlists");
+    const arrayNum = this.state.currentPrayerRequestNum;
+
+    console.log(this.state.currentPrayerRequestNum);
+    console.log(
+      `response.data[0].prayerRequests[arrayNum]: ${response.data[0].prayerRequests[arrayNum]}`
+    );
+
+    const update = await ReadPrayerList.put("/prayerlists", arrayToUpdate);
   };
 
   componentDidMount() {
@@ -78,9 +143,49 @@ class EditPrayerList extends Component {
         style={styles.backgroundImage}
       >
         <View style={styles.container}>
-          <WrappedText style={styles.stylingOfWrap}>
-            {this.getPrayersToDisplay(this.state.prayerList)}
-          </WrappedText>
+          <Card>
+            <View>
+              <Text style={styles.textHeading}>Praying for:</Text>
+              <TextInput
+                placeholder={this.state.prayerTheme}
+                onChangeText={(text) => this.updateTheme(text)}
+              />
+              <Card.Divider />
+              {this.getPrayersToDisplay(this.state.prayerList)}
+            </View>
+          </Card>
+
+          <Card>
+            <View style={styles.fixToText}>
+              <Pressable
+                style={styles.buttonUpdateAndDelete}
+                onPress={this.updatePrayerRequestsAction}
+              >
+                <Text style={styles.textButtons}>Update</Text>
+              </Pressable>
+              <Pressable style={styles.buttonUpdateAndDelete}>
+                <Text style={styles.textButtons}>Delete</Text>
+              </Pressable>
+            </View>
+          </Card>
+
+          <Card>
+            <View style={styles.fixToText}>
+              <Pressable
+                style={styles.buttonNextAndPrev}
+                onPress={this.skipToThePreviousPrayerRequest}
+              >
+                <Text>Previous</Text>
+              </Pressable>
+
+              <Pressable
+                style={styles.buttonNextAndPrev}
+                onPress={this.skipToTheNextPrayerRequest}
+              >
+                <Text>Next</Text>
+              </Pressable>
+            </View>
+          </Card>
         </View>
       </ImageBackground>
     );
@@ -120,6 +225,15 @@ const styles = StyleSheet.create({
     backgroundColor: "lightblue",
     padding: 5,
     borderRadius: 7,
+  },
+  buttonUpdateAndDelete: {
+    elevation: 3,
+    backgroundColor: "royalblue",
+    padding: 5,
+    borderRadius: 8,
+  },
+  textButtons: {
+    color: "white",
   },
 });
 
